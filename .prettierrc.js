ee8c5312ebd04f253dd1509492f1371203b40eaf
Alireza Goudarzi <alireza@goudarzi.ws>
2023-01-18 18:29:39 +0200

    install project and required dependencies

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  useTabs: true,
+  tabWidth: 4,
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: true,
+  jsxBracketSameLine: true,
+  printWidth: 80,
+  arrowParens: 'avoid',
+};
